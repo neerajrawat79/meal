ed19bcc7a95504992221919f00bd055bc26c890e
sushil-pandey-kel <sushil.pandey@kellton.com>
2024-06-24 11:14:49 +0530

    Updates

---
+++ precache-manifest.76eb620050bdf1c8fa42b087a421beb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371e6162c3464ed3e47f8eb246ed9d50",
+    "url": "/sushil-pandey-kel/meal-demo/index.html"
+  },
+  {
+    "revision": "9b12ff2ee2446fbbc118",
+    "url": "/sushil-pandey-kel/meal-demo/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "a55dd019b983be7ea4e5",
+    "url": "/sushil-pandey-kel/meal-demo/static/css/main.834abe0e.chunk.css"
+  },
+  {
+    "revision": "9b12ff2ee2446fbbc118",
+    "url": "/sushil-pandey-kel/meal-demo/static/js/2.d809a649.chunk.js"
+  },
+  {
+    "revision": "a55dd019b983be7ea4e5",
+    "url": "/sushil-pandey-kel/meal-demo/static/js/main.068ecfbf.chunk.js"
+  },
+  {
+    "revision": "ed1a96c3c0b9201ac5d4",
+    "url": "/sushil-pandey-kel/meal-demo/static/js/runtime~main.4f9694da.js"
+  }
+]);
